72cd3f498583ae25b773717b99c2e681645682af
parispep <chikeperpetual@gmail.com>
2025-11-17 16:56:33 +0100

    New Project

---
+++ projects/calculator.js
@@ -0,0 +1,39 @@
+// alert("Welcome to my third  Calculator Project!");
+
+function calculate() {
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    alert("please enter valid numbers!")
+    return;
+  }
+
+
+  let operation = document.getElementById("operation").value;
+  let result;
+
+
+
+  if (operation === "add") {
+    result = num1 + num2;
+
+  } else if (operation === "subtract") {
+    result = num1 - num2;
+
+  } else if (operation === "multiply") {
+    result = num1 * num2;
+
+  } else if (operation === "divide") {
+    if (num2 === 0) {
+      result = "cannot divide by zeros";
+    } else {
+      result = num1 / num2;
+    }
+  }
+
+  document.getElementById('result').innerText = "Result: " + result;
+
+}
+
+document.getElementById("calculateBtn").addEventListener('click', calculate);
